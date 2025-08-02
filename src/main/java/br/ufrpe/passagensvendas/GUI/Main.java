df5de9f912a13d3947b1790ba3bb270b19610d39
ThiagoRozendo <thiago.rozendo@ufrpe.br>
2025-08-01 21:13:41 -0300

    feat: adicao da aplicacao main

---
+++ src/main/java/br/ufrpe/passagensvendas/GUI/Main.java
@@ -0,0 +1,24 @@
+package br.ufrpe.passagensvendas.GUI;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/TelaPrincipal.fxml"));
+        primaryStage.setTitle("Sistema de Venda de Passagens Aéreas");
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
